424d7536d7cc784c1a3a7d07dd6a6fe4b002f4db
Rafael <88090582+rafa-san@users.noreply.github.com>
2024-08-26 14:47:06 -0300

    Inserção de novos rascunhos

---
+++ rascunho.js
@@ -0,0 +1,24 @@
+/*
+let nome = "Rafael";
+let letraR = nome.charCodeAt(0);
+
+if(letraR === 82) {
+  console.log("Letra R");
+} else {
+  console.log("Não é a letra R");
+}
+
+
+
+let nome;
+console.log(nome);
+
+
+
+console.log(15 % 9);
+*/
+
+let x = 5;
+x++;
+let z = x;
+console.log(z);
